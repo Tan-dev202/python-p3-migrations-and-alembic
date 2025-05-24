26642d6141b406db13da4470ecdeb2beb20cdbb8
Enock Tangus <tanguskipngeno@gmail.com>
2025-05-24 11:36:42 +0300

    migrations and alembic

---
+++ lib/migrations/versions/1698a1738400_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1698a1738400
+Revises: 
+Create Date: 2025-05-24 11:20:40.051594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1698a1738400'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
